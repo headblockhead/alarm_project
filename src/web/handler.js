dce3eba05f2cce6201c55c96914355acaf692c02
Adrian Hesketh <adrianhesketh@hushmail.com>
2018-08-22 10:05:20 +0100

    Give permission to send email.

--- src/web/handler.js
+++ src/web/handler.js
@@ -4,20 +4,25 @@ var AWS = require('aws-sdk');
 
 module.exports.detect = detect;
 
+const response = {
+  statusCode: 200,
+  body: JSON.stringify({ ok: true }),
+};
+
 function detect(event, context, callback) {
   console.log(event.body);
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({ ok: true }),
-  };
 
   var e = JSON.parse(event.body);
 
-  sendEmail(process.env.FROM_EMAIL_ADDRESS, process.env.TO_EMAIL_ADDRESS, "Door", e.event)
-    .then(function(data) {
+  var from = process.env.FROM_EMAIL_ADDRESS;
+  var to = process.env.TO_EMAIL_ADDRESS;
+  console.log({ "msg": "sending email", from, to });
+
+  sendEmail(from, to, "Door", e.event)
+    .then(function (data) {
       console.log({ emailsSent: 1 });
     })
-    .catch(function(err) {
+    .catch(function (err) {
       console.log({ msg: "sending email failed", err });
     });
 
@@ -25,26 +30,26 @@ function detect(event, context, callback) {
 }
 
 function sendEmail(from, to, subject, body) {
-  AWS.config.update({region: 'eu-west-1'});
+  AWS.config.update({ region: 'eu-west-1' });
   var params = {
     Destination: {
-      ToAddresses: [ to ]
+      ToAddresses: [to]
     },
     Message: {
       Body: {
         Text: {
-         Charset: "UTF-8",
-         Data: body
+          Charset: "UTF-8",
+          Data: body
         }
-       },
-       Subject: {
+      },
+      Subject: {
         Charset: 'UTF-8',
         Data: subject
-       }
-      },
+      }
+    },
     Source: from,
-  };       
-  
-  var ses = new AWS.SES({apiVersion: '2010-12-01'});
-  return ses.sendEmail(params);
+  };
+
+  var ses = new AWS.SES({ apiVersion: '2010-12-01' });
+  return ses.sendEmail(params).promise();
 }
